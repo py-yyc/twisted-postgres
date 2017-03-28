2530e51afc5a5d14a7cd8c47cc7616dbfbcbee67
meejah <meejah@meejah.ca>
2017-03-28 14:36:04 -0600

    logos are fun

--- code-slides/0000-intro.py
+++ code-slides/0000-intro.py
@@ -1,4 +1,6 @@
 ##   <h1 style="valign:center;">txpostgres</h1>
-##  <div class="incremental" style="text-align:center;"><img height="350" width="auto" src="avatar-transparent.svg" />
-##    <p>mike warren<br/><span class="small">https://<span style="color:#c33; font-size: 125%; font-family: 'source code pro' monospace; text-shadow: 1px 1px black;">meejah.ca</span></span></p>
+##  <div class="incremental" style="text-align:center;"><img height="25%" width="auto" src="avatar-transparent.svg" />
+##    <br/><span class="small">https://<span style="color:#c33; font-size: 125%; font-family: 'source code pro' monospace; text-shadow: 1px 1px black;">meejah.ca</span></span></p>
+##    <img height="auto" width="40%" src="crossbar_icon_and_text_vectorized_yellow_icon.svg" style="margin: 5%;" />
+##    <img height="auto" width="20%" src="la-logo.png" style="background-color:#eee; padding: 10px; margin: 5%;" />
 ##  </div>
